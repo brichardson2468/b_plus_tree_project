d3e5a82d187bb67f49ab4a07bb4c9b0efc29c048
Brad Richardson <brichardson2468@gmail.com>
2024-04-29 11:27:36 -0400

    convert to datetime

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ def load_data(filepath):
     try:
         data = pd.read_csv(filepath)
         print("Data loaded successfully!")
+        data['Time Period Start Date'] = pd.to_datetime(data['Time Period Start Date'])
+        data['Time Period End Date'] = pd.to_datetime(data['Time Period End Date'])
         print(data.head())  # Display the first few rows
+        print(data.dtypes)  # Display the data types of each column
     except FileNotFoundError:
         print("Error: The file was not found.")
     except Exception as e:

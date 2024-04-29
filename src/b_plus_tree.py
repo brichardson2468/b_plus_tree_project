2c6376a9bcf9de412ec75fc37f36734845b8d148
Brad Richardson <brichardson2468@gmail.com>
2024-04-29 15:04:56 -0400

    _split_root and _find_leaf

--- src/b_plus_tree.py
+++ src/b_plus_tree.py
@@ -21,10 +21,26 @@ class BPlusTree:
         """Search for all records within a specified range."""
         pass
 
-    def _split_root(self):
-        """Split the root if necessary."""
-        pass
+    def _split_root(self): # Helper method to split the root node
+        old_root = self.root
+        # Split the old root
+        new_right_node, mid_key = old_root.split()
 
-    def _find_leaf(self, key):
-        """Helper method to find the leaf node that should contain the key."""
-        pass
+        # Create a new root node which is not a leaf
+        new_root = TreeNode(is_leaf=False)
+        new_root.keys = [mid_key]
+        new_root.children = [old_root, new_right_node]  # old_root is now the new_left_node
+
+        # Set the new root as the tree's root
+        self.root = new_root
+
+    def _find_leaf(self, key): # Helper method to find the leaf node where the key should be inserted
+        current_node = self.root
+        while not current_node.is_leaf:  # Continue until a leaf node is reached
+            # Traverse through the keys in the node to find appropriate child
+            i = 0
+            while i < len(current_node.keys) and key >= current_node.keys[i]:
+                i += 1
+            # Move to the next child node
+            current_node = current_node.children[i]
+        return current_node
